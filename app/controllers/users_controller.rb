181d2cbaedcc6c53f5e6a2f507bd88b4d9ff6206
yuta <yuta.s1182@gmail.com>
2019-06-19 22:10:28 +0900

    [add]test追加
    アドミンのテストを追加したため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destory
 
   def index
     @users = User.paginate(page: params[:page])
@@ -63,5 +64,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
